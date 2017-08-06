b00165a9ddf461db03baa344272f018dacc66df8
Dale Arney <acedarney@gmail.com>
2017-08-06 16:12:10 -0400

    Create working map

--- wall_hub.py
+++ wall_hub.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('header_footer.html')
+    with open('api_key.txt') as f:
+        key = f.read()
+    return render_template('header_footer.html', key=key)
 
 
 if __name__ == '__main__':
