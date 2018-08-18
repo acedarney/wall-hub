8ffe295b8e8b0503c71a9fcc2d698d6049407293
Dale Arney <Dale.C.Arney@nasa.gov>
2018-08-18 17:45:24 -0400

    Create structure for pages app

--- wallhub/pages/urls.py
+++ wallhub/pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ToDoView, WeatherView, CalendarView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index-page'),
+    path('todo', ToDoView.as_view(), name='todo-page'),
+    path('weather', WeatherView.as_view(), name='weather-page'),
+    path('calendar', CalendarView.as_view(), name='calendar-page')
 ]
--- wallhub/pages/views.py
+++ wallhub/pages/views.py
@@ -2,3 +2,12 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class ToDoView(TemplateView):
+    template_name = 'todo.html'
+
+class WeatherView(TemplateView):
+    template_name = 'weather.html'
+
+class CalendarView(TemplateView):
+    template_name = 'calendar.html'
